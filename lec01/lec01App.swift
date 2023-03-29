b5db03fa5e20a13e9a0e6d387f7abe1d1da0c223
AuTtx <1932063838@qq.com>
2023-03-29 19:09:20 +0800

    Initial Commit

---
+++ lec01/lec01App.swift
@@ -0,0 +1,17 @@
+//
+//  lec01App.swift
+//  lec01
+//
+//  Created by 许智尧 on 2023/3/29.
+//
+
+import SwiftUI
+
+@main
+struct lec01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
